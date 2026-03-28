3a5eb2855b6d990d9dc99be62ba6b9ad9ebd2db4
Anuj Chawla <anujchawla@Anujs-MacBook-Pro.local>
2026-03-28 13:35:18 -0300

    Initial Commit

---
+++ chain.js
@@ -0,0 +1,28 @@
+import { config } from "dotenv";
+config();
+
+import { PromptTemplate } from "@langchain/core/prompts";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+const model = new ChatGoogleGenerativeAI({
+  model: "gemini-2.5-flash",
+  temperature: 0.9,
+  apiKey: process.env.GOOGLE_API_KEY,
+  maxOutputTokens: 1024,
+});
+
+const prompt = PromptTemplate.fromTemplate(
+  "Answer the following question: {question}"
+);
+
+const chain = prompt.pipe(model);
+
+try {
+  const response = await chain.invoke({
+    question: "tell me business model for global solutions team company based out in Etobicoke, Ontario",
+  });
+
+  console.log(response.content);
+} catch (err) {
+  console.error(err);
+}
